aebfe8d64236cb7bfe849f750b50a1cdfae503c3
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-06-07 12:13:20 +0300

    determine TokenizationCampaign main functions

--- contracts/src/AssetToken.sol
+++ contracts/src/AssetToken.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.28;
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+/// @title AssetToken contract
+/// @author AlinCiprian
+/// @notice This contract will be deployed once for every asset that will be tokenized. Upon deployment, 100 tokens - each representing
+/// 1% share of the asset - are minted to the TokenizationCampaign contract.
+
 contract AssetToken is ERC20, Ownable {
     /// The maximum amount of tokens that will ever be is 100, each representing 1% stake in the asset.
     uint256 public constant MAX_SUPPLY = 100 ether;
@@ -12,6 +17,7 @@ contract AssetToken is ERC20, Ownable {
         _mint(msg.sender, MAX_SUPPLY);
     }
 
+    /// This function is meant to be called once the item is sold in real life and the tokens no longer have any use.
     function burnTokens() external onlyOwner {
         _burn(msg.sender, MAX_SUPPLY);
     }
